034915e8c37c6ca17b81198ac8bcf3041c74f75d
soareswallace <wssj2@cin.ufpe.br>
2018-07-12 23:47:43 -0300

    Planes flying and dropping after touching

--- Monst-AR/ViewController.swift
+++ Monst-AR/ViewController.swift
@@ -14,9 +14,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     var trackerNode: SCNNode!
+    var mainContainer: SCNNode!
     var gameHasStarted = false
     var foundSurface = false
     var gamePos = SCNVector3Make(0.0, 0.0, 0.0)
+    
+    var score = 0 {
+        didSet{
+            scoreLabel.text =
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +56,87 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func randomPosition() -> SCNVector3{
+        let randomX = (Float(arc4random_uniform(200))/100.0) - 1.0
+        let randomY = (Float(arc4random_uniform(200))/100.0) + 1.5
+        
+        return SCNVector3Make(randomX, randomY, -3.0)
+    }
+    
+    @objc func addPlane() {
+        let planeNode = sceneView.scene.rootNode.childNode(withName: "plane", recursively: false)?.copy() as! SCNNode
+        planeNode.isHidden = false
+        planeNode.position = randomPosition()
+        
+        mainContainer.addChildNode(planeNode)
+        
+        let randSpeed = SCNVector3Make(0.0, 0.0, Float(arc4random_uniform(2) + 4))
+        planeNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        planeNode.physicsBody?.isAffectedByGravity = false
+        planeNode.physicsBody?.applyForce(randSpeed, asImpulse: true)
+        
+        let planeDissapearAction = SCNAction.sequence([SCNAction.wait(duration: 10.0), SCNAction.fadeOut(duration: 1.0), SCNAction.removeFromParentNode()])
+        planeNode.runAction(planeDissapearAction)
+        
+        Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(addPlane), userInfo: nil, repeats: false)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if gameHasStarted{
+            //Plane Crashsing stuf later in part 3
+            guard let touch = touches.first else { return }
+            
+            let touchLocation = touch.location(in: view)
+            
+            guard let nodeHitTest = sceneView.hitTest(touchLocation, options: nil).first else { return }
+            let hitNode = nodeHitTest.node
+            
+            guard hitNode.name == "plane" else { return }
+            
+            let planeSpinForce = SCNVector4Make(0.5, 0.0, 1.0, 50)
+            hitNode.physicsBody?.isAffectedByGravity = true
+            hitNode.physicsBody?.applyTorque(planeSpinForce, asImpulse: true)
+            
+        }
+        else{
+            guard foundSurface else { return }
+            trackerNode.removeFromParentNode()
+            gameHasStarted = true
+            
+            scoreLabel = UILabel(frame: CGRect()
+            
+            mainContainer = sceneView.scene.rootNode.childNode(withName: "mainContainer", recursively: false)!
+            mainContainer.isHidden = false
+            mainContainer.position = gamePos
+            
+            addPlane()
+            
+            let ambientLight = SCNLight()
+            ambientLight.type = .ambient
+            ambientLight.color = UIColor.white
+            ambientLight.intensity = 1000
+            
+            let ambientLightNode = SCNNode()
+            ambientLightNode.light = ambientLight
+            ambientLightNode.position.y = 3.0
+            
+            mainContainer.addChildNode(ambientLightNode)
+            
+            let omniLight = SCNLight()
+            omniLight.type = .omni
+            omniLight.color = UIColor.white
+            omniLight.intensity = 500
+            
+            let omniLightNode = SCNNode()
+            omniLightNode.light = omniLight
+            omniLightNode.position.y = 3.0
+            
+            mainContainer.addChildNode(omniLightNode)
+            
+        }
     }
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
